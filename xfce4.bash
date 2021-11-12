3c687e66006909935c9eac6dd588d11ab2a3373d
LowTechCoder <73705040+LowTechCoder@users.noreply.github.com>
2021-11-12 10:56:40 -0500

    Update xfce4.bash

--- xfce4.bash
+++ xfce4.bash
@@ -1,11 +1,9 @@
 sudo apt install neovim -y
-sudo apt install xfce4 -y
-sudo apt install xfce4-terminal -y
 curl -sSL https://git.io/JfAPE | bash
-#sudo apt remove lx* -y
 git clone https://lowtechcoder/linux-nvim
 mkdir .config/nvim/
 cp linux-nvim/init.vim .config/nvim/
-#sudo apt autoremove -y
-#sudo apt autoclean -y
+sudo apt autoremove -y
+sudo apt autoclean -y
+sudo update-alternatives --config x-window-manager
 sudo reboot
